c88e0649ce1c668b5b295aa05eec7d4a2f9a90d2
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-08-17 17:55:22 -0500

    Update hashtag view to use the new classifier and collector

--- clasificacion/views.py
+++ clasificacion/views.py
@@ -1,43 +1,20 @@
 from django.shortcuts import render
 
-import tweepy
-
 from .classifiers.classifier import LogisticRegressionClassifier
-from .classifiers.classifier import LSTMClassifier
+from .classifiers.collector import collect_tweets
 
-
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
 
 logit_classifier = LogisticRegressionClassifier()
-lstm_classifier = LSTMClassifier()
+
 
 def index(request):
     return render(request, 'clasificacion/index.html')
 
+
 def hashtag(request):
     if request.method == 'POST':
-        hasht = request.POST['hashtag']
-        query = f'{hasht} -filter:retweets'
-        date = f"{request.POST['anioDesde']}-{request.POST['mesDesde']}-{request.POST['diaDesde']}"
-
-        data = []
-        cursor = tweepy.Cursor(api.search, tweet_mode='extended', q=query, count=100, lang='es', since=date)
-        for tweet in cursor.items(int(request.POST['cantidad'])):
-            id_str = tweet._json['id_str']
-            tweet._json['id_url'] = f'https://twitter.com/twitter/statuses/{id_str}'
-            data.append(tweet._json)
-
-        if request.POST['model'] == 'lg' :
-            prediction = logit_classifier.classify(data)
-        else:
-            prediction = lstm_classifier.classify(data)
+        data = collect_tweets()
+        prediction = logit_classifier.classify(data)
 
         if len(prediction) == 0:
             context = { 'info': 'No existen tweets que cumplan con los parámetros de búsqueda ingresados.' }
@@ -51,6 +28,7 @@ def hashtag(request):
         return render(request, 'clasificacion/hashtag.html', context)
     else:
         return render(request, 'clasificacion/hashtag.html')
+
 
 def tweet(request):
     return render(request, 'clasificacion/tweet.html')
