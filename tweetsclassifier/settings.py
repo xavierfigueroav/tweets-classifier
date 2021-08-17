b0d9689f07b91bad397e2d9d374b6d1df1cb5e52
Xavier Figueroa <xavierfigueroav@gmail.com>
2021-08-17 18:11:14 -0500

    Get Django secret key from environment variables

--- tweetsclassifier/settings.py
+++ tweetsclassifier/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@-r_1^uv@5f)&lnmiogy2=^3y_d6f=h8s=e1&#n+&mpi7)-3t0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@-r_1^uv@5f)&lnmiogy2=^3y_d6f=h8s=e1&#n+&mpi7)-3t0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
